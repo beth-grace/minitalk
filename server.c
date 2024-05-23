a68af714291d2e201b7704f95ae48006538e77c3
Beth Laptop <beefie@Beefie>
2024-05-23 22:00:08 +0930

    tried bit to byte reader from home

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 17:12:42 by bmilford          #+#    #+#             */
-/*   Updated: 2024/05/23 18:14:24 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/05/23 21:59:20 by beefie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,26 @@ int main()
 
 void	sig_handler(int sig)
 {
-	if (sig == SIGUSR1)
-		ft_printf("RECIEVED SIGUSR1\n");
-	else
-		ft_printf("RECIEVED SIGUSR2\n");
+	//if (sig == SIGUSR1)
+	//	ft_printf("RECIEVED SIGUSR1\n");
+	//else
+	//	ft_printf("RECIEVED SIGUSR2\n");
+	int	size;
+	char	*str;
+
+	while (1)
+	{
+		bit = 7;
+		str = 0; 
+		while (bit >= 0)
+		{
+			if (sig == SIGUSR1)
+				str << bit == 0;
+			else
+				str << bit == 1;
+			bit--;
+
+		}
+		ft_printf(str);
+	}
 }
